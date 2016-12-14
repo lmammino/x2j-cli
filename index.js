8dde094d9d888703cdd7cdfa3e8e293c78ea5ecf
Luciano Mammino <luciano@planet9energy.com>
2016-12-14 10:03:41 +0000

    first working version

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var stdin = process.stdin;
+var stdout = process.stdout;
+var content = '';
+
+var xml2js = require('xml2js');
+
+stdin.resume();
+stdin.setEncoding('utf8');
+
+stdin.on('data', function (chunk) {
+  content += chunk;
+});
+
+stdin.on('end', function () {
+  xml2js.parseString(content, (err, parsedData) => {
+    if (err) {
+      return console.error(err);
+    }
+    var outputJSON = JSON.stringify(parsedData, null, 2);
+    stdout.write(outputJSON);
+    stdout.write('\n');
+  });
+});
